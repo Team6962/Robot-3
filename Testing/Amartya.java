eb5de2531579fab8575b8f86a73b8c641702b6b1
FrostedPyromaniac <39965543+FrostedPyromaniac@users.noreply.github.com>
2021-01-16 10:56:03 -0800

    Update and rename Amartya Robot Code to Amartya.java

--- Testing/Amartya.java
+++ Testing/Amartya.java
@@ -1,10 +1,10 @@
 if ( now - start < 1500 ) {
       myDrive.tankDrive( 0.6,0.6 );
-    else if (now - start < 3500){
+ else if (now - start < 3500){
       myDrive.tankDrive( 0.5,0 );
       myDrive.tankDrive(0.7,-0.7);
-    }
-    else if (now - start < 4500){
+ }
+ else if (now - start < 4500){
       myDrive.tankDrive(0.4, 0.6);
     }
     else if (now - start < 6500){
