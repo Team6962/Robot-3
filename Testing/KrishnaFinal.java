911c4dd14882b8c05d80a00509f1f820b391110f
kkemisetti <krishna.kemisetti@gmail.com>
2021-01-16 15:20:16 -0800

    Update KrishnaFinal.java

--- Testing/KrishnaFinal.java
+++ Testing/KrishnaFinal.java
@@ -4,16 +4,16 @@
     else if(now - start < 1000){
       myDrive.tankDrive(0.5,-0.5);
     }
-    else if(now - start < 2000){
+    else if(now - start < 5000){
       myDrive.TankDrive(0.5,0.5);
     }
-    else if(now - start < 2500){
+    else if(now - start < 5500){
       myDrive.TankDrive(0.65,0.65);
     }
-    else if(now -start < 3500){
+    else if(now -start < 9500){
       myDrive.tankDrive(-0.65,0.65);
     }
-    else if(now - start < 4000){
+    else if(now - start < 10000){
       myDrive.TankDrive(0.8,0.8);
     }
     else{
