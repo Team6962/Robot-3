3e2cf8c0a152808cd73dd081425425dff68df1a4
FrostedPyromaniac <39965543+FrostedPyromaniac@users.noreply.github.com>
2021-01-16 14:53:34 -0800

    Create 24synchro.java

---
+++ Testing/24synchro.java
@@ -0,0 +1,20 @@
+public static void rotateRight(now, begintime, stoptime, speed) {
+  if (now > begintime) {
+    if (now < stoptime) {
+      if (-encoder1.getDistance() == encoder2.getDistance()) {
+          myDrive.tankDrive(speed, -speed);
+      } else if (-encoder1.getDistance() > encoder2.getDistance()) {
+          myDrive.tankDrive(speed * 0.9, -speed * 1.1);
+      } else if (-encoder1.getDistance() < encoder2.getDistance()) {
+          myDrive.tankDrive(speed * 1.1, -speed * 0.9);
+      }
+    } else {
+      myDrive.tankDrive(0, 0);
+    }
+  }
+}
+
+
+
+rotateRight(now, 0, 1250, /*[TBD]*/);
+// Please test TBD for different values... We want the value that makes the robot rotate 90 degrees.
