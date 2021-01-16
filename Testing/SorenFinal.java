ea6dcb29fd6148b4a55cc5d118f2b4e4340384fb
Soren <soren@aethre.co>
2021-01-16 15:34:53 -0800

    Update SorenFinal.java

--- Testing/SorenFinal.java
+++ Testing/SorenFinal.java
@@ -1,15 +1,16 @@
 long now = System.currentTimeMillis();
 double speed = 0.65;
-if (now - start > 7000) { // stop after seven seconds
+double time = now - start;
+if (time > 7000) { // stop after seven seconds
     myDrive.tankDrive(0, 0);
-} else if (now - start % 2000 > 1000) { // otherwise do a lil dance that loops every 2 seconds
-    if (now - start % 500 > 250) {
+} else if ((now - start % 2000) > 1000) { // otherwise do a lil dance that loops every 2 seconds
+    if ((time % 500) > 250) {
         myDrive.tankDrive(speed, -speed);
     } else {
         myDrive.tankDrive(-speed, speed);
     }
 } else {
-    if (now - start % 500 > 250) {
+    if ((time % 500) > 250) {
         myDrive.tankDrive(-speed, -speed);
     } else {
         myDrive.tankDrive(speed, speed);
