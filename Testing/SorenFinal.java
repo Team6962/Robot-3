7e0eee848817ac45a29f700661bc81c1bcc62789
Soren <soren@aethre.co>
2021-01-16 15:02:20 -0800

    Update SorenFinal.java

--- Testing/SorenFinal.java
+++ Testing/SorenFinal.java
@@ -3,19 +3,19 @@ long time = (now - start) % 2000;
 if (now - start > 7000) { // stop after ten seconds
     myDrive.tankDrive(0, 0);
 } else if (time > 1750) { // otherwise do a lil dance that loops every 2 seconds
-    myDrive.tankDrive(0.4, -0.4);
+    myDrive.tankDrive(0.65, -0.65);
 } else if (time > 1500) {
-    myDrive.tankDrive(-0.4, 0.4);
+    myDrive.tankDrive(-0.65, 0.65);
 } else if (time > 1250) {
-    myDrive.tankDrive(0.4, -0.4);
+    myDrive.tankDrive(0.65, -0.65);
 } else if (time > 1000) {
-    myDrive.tankDrive(-0.4, 0.4);
+    myDrive.tankDrive(-0.65, 0.65);
 } else if (time > 750) {
-    myDrive.tankDrive(0.4, 0.4);
+    myDrive.tankDrive(0.65, 0.65);
 } else if (time > 500) {
-    myDrive.tankDrive(-0.4, -0.4);
+    myDrive.tankDrive(-0.65, -0.65);
 } else if (time > 250) {
-    myDrive.tankDrive(0.4, 0.4);
+    myDrive.tankDrive(0.65, 0.65);
 } else if (time > 0) {
-    myDrive.tankDrive(-0.4, -0.4);
+    myDrive.tankDrive(-0.65, -0.65);
 }
