03adb39bf0186b38156482a20f2372e98a3a2600
FrostedPyromaniac <39965543+FrostedPyromaniac@users.noreply.github.com>
2021-01-16 11:01:59 -0800

    Update Amartya.java

--- Testing/Amartya.java
+++ Testing/Amartya.java
@@ -1,17 +1,17 @@
 if ( now - start < 1500 ) {
-      myDrive.tankDrive( 0.6,0.6 );
+      myDrive.tankDrive( 0.8,0.8 );
  else if (now - start < 3500){
-      myDrive.tankDrive( 0.5,0 );
+      myDrive.tankDrive( 0.7,0 );
       myDrive.tankDrive(0.7,-0.7);
  }
  else if (now - start < 4500){
-      myDrive.tankDrive(0.4, 0.6);
+      myDrive.tankDrive(0.4, 0.8);
     }
     else if (now - start < 6500){
-      myDrive.tankDrive(0.5, 0.2);
+      myDrive.tankDrive(0.8, 0.4);
     }
     else if (now - start < 8000){
-      myDrive.tankDrive(0.6,-0.6);
+      myDrive.tankDrive(0.8,-0.8);
     }
     else{
       myDrive.tankDrive(0,0);
