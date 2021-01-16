48baf1385c225ad2d9bc5bba2ed23476ee6c0fe6
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2021-01-16 15:35:34 -0800

    Update 24synchro.java

--- Testing/24synchro.java
+++ Testing/24synchro.java
@@ -1,20 +1,22 @@
-public static void rotateRight(int now, int begintime, int stoptime, double speed) {
+public static double[] rotateRight(int now, int begintime, int stoptime, double speed) {
+  double[] values = new double[2];
   if (now > begintime) {
     if (now < stoptime) {
       if (-encoder1.getDistance() == encoder2.getDistance()) {
-          myDrive.tankDrive(speed, -speed);
+          values = [speed, -speed];
       } else if (-encoder1.getDistance() > encoder2.getDistance()) {
-          myDrive.tankDrive(speed * 0.9, -speed * 1.1);
+          values = [speed * 0.9, -speed * 1.1];
       } else if (-encoder1.getDistance() < encoder2.getDistance()) {
-          myDrive.tankDrive(speed * 1.1, -speed * 0.9);
+          values = [speed * 1.1, -speed * 0.9];
       }
     } else {
-      myDrive.tankDrive(0, 0);
+      values = [0.0, 0.0];
     }
   }
 }
 
 
 
-rotateRight(now, 0, 1250, /*[TBD]*/);
-// Please test TBD for different values... We want the value that makes the robot rotate 90 degrees.
+double[] values = rotateRight(now, 0, 1250, /*[TBD]*/);
+myDrive.tankDrive(values[0], values[1]);
+// Please test TBD for different values... We want the value that makes the robot rotate 90 degrees after 1.25 sec.
