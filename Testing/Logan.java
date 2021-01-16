9363bef0482b5a17a4999cb170d0358faeef5b59
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2021-01-16 10:51:21 -0800

    Create Logan.java

---
+++ Testing/Logan.java
@@ -0,0 +1,21 @@
+if (now - start < 1500) {
+  myDrive.tankDrive(0.5, 0.5); // Move forward for 1.5 sec
+} else if (now - start < 2000) {
+  myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
+} else if (now - start < 3000) {
+  myDrive.tankDrive(-1, 1);    // Turn left for 1 sec
+} else if (now - start < 3500) {
+  myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
+} else if (now - start < 5500) {
+  myDrive.tankDrive(1, -1);    // Turn right for 2 sec
+} else if (now - start < 6000) {
+  myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
+} else if (now - start < 7000) {
+  myDrive.tankDrive(-1, 1);    // Turn left for 1 sec
+} else if (now - start < 7500) {
+  myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
+} else if (now - start < 9000) {
+  myDrive.tankDrive(-0.5, -0.5);   // Move Backwards for 1.5 sec
+} else {
+  myDrive.tankDrive(0, 0);
+}
