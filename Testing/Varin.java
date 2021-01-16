ad7e2117086a063a6b8a4aca6bb8a188039d169a
anawesomebear <77546600+anawesomebear@users.noreply.github.com>
2021-01-16 11:31:27 -0800

    Create Varin.java

---
+++ Testing/Varin.java
@@ -0,0 +1,15 @@
+@Override
+  public void autonomousPeriodic() {
+
+    long now = System.currentTimeMillis();
+    if (now - start < 2000) {
+      myDrive.tankDrive(-0.9, 0.9);
+    } else if (now - start < 4000) {
+      myDrive.tankDrive(0.7, -0.7);
+    } else if (now - start < 6000) {
+      myDrive.tankDrive(0.6, 0.6);
+    } else if (now - start < 8000) {
+      myDrive.tankDrive(-0.8, -0.8);
+    }
+
+  }
