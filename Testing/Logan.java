03bea54295a3dddc64a99755e952eb836be21e68
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2021-01-16 11:03:08 -0800

    Update Logan.java

--- Testing/Logan.java
+++ Testing/Logan.java
@@ -1,21 +1,21 @@
 if (now - start < 1500) {
-  myDrive.tankDrive(0.5, 0.5); // Move forward for 1.5 sec
+  myDrive.tankDrive(0.8, 0.8); // Move forward for 1.5 sec
 } else if (now - start < 2000) {
   myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
 } else if (now - start < 3000) {
-  myDrive.tankDrive(-0.5, 0.5);    // Turn left for 1 sec
+  myDrive.tankDrive(-0.8, 0.8);    // Turn left for 1 sec
 } else if (now - start < 3500) {
   myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
 } else if (now - start < 5500) {
-  myDrive.tankDrive(0.5, -0.5);    // Turn right for 2 sec
+  myDrive.tankDrive(0.8, -0.8);    // Turn right for 2 sec
 } else if (now - start < 6000) {
   myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
 } else if (now - start < 7000) {
-  myDrive.tankDrive(-0.5, 0.5);    // Turn left for 1 sec
+  myDrive.tankDrive(-0.8, 0.8);    // Turn left for 1 sec
 } else if (now - start < 7500) {
   myDrive.tankDrive(0, 0);     // Pause for 0.5 sec
 } else if (now - start < 9000) {
-  myDrive.tankDrive(-0.5, -0.5);   // Move Backwards for 1.5 sec
+  myDrive.tankDrive(-0.8, -0.8);   // Move Backwards for 1.5 sec
 } else {
   myDrive.tankDrive(0, 0);
 }
