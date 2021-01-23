e8e7b54f73b20806dc88e3c6e16c97a13960ecd2
Shisket <50220591+Shisket@users.noreply.github.com>
2021-01-23 12:22:41 -0800

    Update ShiraRecord.java

--- ReplayAndRecordingTest/ShiraRecord.java
+++ ReplayAndRecordingTest/ShiraRecord.java
@@ -7,13 +7,37 @@ public void autonomousPeriodic() {
   encoder1.reset();
   encoder2.reset();
 
-  ArrayList<double[]> encoderValues = new ArrayList<double[]>();
+  ArrayList<String[]]> encoderValues = new ArrayList<String[]>();
   //Set an interval to record at (every 0.05)
-  if ((now-start) % 50 == 0) {
-    encoderValues.add(new double[] {encoder1.getDistance(), encoder2.getDistance()});
+  while ((now-start) <= 60000) {
+    long now = System.currentTimeMillis();
+    if ((now-start) % 50 == 0) {
+      encoderValues.add(new String[] {String.valueOf(encoder1.getDistance()), String.valueOf(encoder2.getDistance())});
+    }
+    long now = System.currentTimeMillis();
   }
   //Grab encoder at that time
   //ArrayList gets encoder
   //Arraylist is written to CSV file
-  //IDK how to write to a CSV file
+  try {			
+    FileWriter csvWriter = new FileWriter("\\Users\\mathi\Desktop\\test.csv");
+    csvWriter.append("Links, AI Summary \n");
+    
+    for (int i = 0; i < encoderValues.size(); i++) {
+      for (int j = 0; j < encoderValues.get(i).length; j++) {
+        csvWriter.append(encoderValues.get(i)[j]);
+        
+        if (j != encoderValues.get(i).length-1) {
+          csvWriter.append(", ");
+        }
+      }
+      csvWriter.append("\n");
+    }
+    
+    csvWriter.flush();
+    csvWriter.close();
+  } 
+  catch (Exception e) {
+    System.out.println(e + "ERROR IN WRITING FILE.");
+  }
  }
