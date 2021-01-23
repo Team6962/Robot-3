59426f87d18bb1670a68b819b6fb22cad842cf35
Shisket <50220591+Shisket@users.noreply.github.com>
2021-01-23 15:34:45 -0800

    Update Shira recordValues.java

--- ReplayAndRecordingTest/Shira recordValues.java
+++ ReplayAndRecordingTest/Shira recordValues.java
@@ -2,14 +2,14 @@ public void recordValues(encoder1, encoder2, arraylist) {
   if ((now-start) % 50 == 0) {
     arraylist.add(new String[] {String.valueOf(encoder1.getDistance()), String.valueOf(encoder2.getDistance())});
   }
-  if(joystick1.getRawButton(1)) {
+  if(joystick1.getRawButton(12)) {
     try {			
         FileWriter csvWriter = new FileWriter("test.csv");
         csvWriter.append("Links, AI Summary \n");
         
         for (int i = 0; i <arraylist.size(); i++) {
           for (int j = 0; j <arraylist.get(i).length; j++) {
-            csvWriter.append(al.get(i)[j]);
+            csvWriter.append(arraylist.get(i)[j]);
             
             if (j !=arraylist.get(i).length-1) {
               csvWriter.append(", ");
