d7c7a5e4ee80d526f6a265c75e4b1a6d5214abe6
Soren <soren@aethre.co>
2021-01-23 14:37:16 -0800

    do it better

--- ReplayAndRecordingTest/SorenReplay.java
+++ ReplayAndRecordingTest/SorenReplay.java
@@ -2,7 +2,7 @@
 // Autonomous
 int clock;
 long start;
-int[][] path;
+ArrayList<int[]> path;
 double correctionFactor;
 */
 
@@ -11,7 +11,19 @@ public void autonomousInit() {
     start = System.currentTimeMillis();
     clock = 500;
     correctionFactor = 0.25;
-    path = new int[][] { { 0, 0 }, { 0, 5 }, { 10, 20 }, { 20, 20 } };
+    path = new ArrayList<int[]>();
+
+    Scanner sc;
+    try {
+        sc = new Scanner(new File("[FILEPATH]"));
+    } catch (Exception e) {
+        sc = new Scanner("0,0\n0,5\n10,20\n20,25\n30,30");
+    }
+    sc.useDelimiter(",");
+    while (sc.hasNext()) {
+        path.add(new int[] { sc.nextInt(), sc.nextInt() });
+    }
+    sc.close();
 }
 
 @Override
@@ -19,20 +31,48 @@ public void autonomousPeriodic() {
     long now = (System.currentTimeMillis() - start);
 
     int step = (int) Math.floor(now / clock); // index of path we're on or going through
-    int[] current = path[step];
-    int[] next = path[step + 1];
+    if (step < path.size() - 1) {
+        int[] current = path.get(step);
+        int[] next = path.get(step + 1);
 
-    double substep = (now % clock) / clock; // % of the way through current path step
+        double substep = (now % clock) / clock; // % of the way through current path step
 
-    double targetLeft = (next[0] - current[0]) * substep;
-    int realLeft = encoder1.get() - current[0];
-    double correctionLeft = targetLeft / realLeft;
-    correctionLeft = ((correctionLeft - 1) * correctionFactor) + 1;
+        double targetLeft = (next[0] - current[0]) * substep;
+        int realLeft = encoder1.get() - current[0];
+        if (realLeft == 0)
+            realLeft = 1;
+        double correctionLeft = targetLeft / realLeft;
+        correctionLeft = ((correctionLeft - 1) * correctionFactor) + 1;
 
-    double targetRight = (next[0] - current[0]) * substep;
-    int realRight = encoder2.get() - current[0];
-    double correctionRight = targetRight / realRight;
-    correctionRight = ((correctionRight - 1) * correctionFactor) + 1;
+        double targetRight = (next[1] - current[1]) * substep;
+        int realRight = encoder2.get() - current[1];
+        if (realRight == 0)
+            realRight = 1;
+        double correctionRight = targetRight / realRight;
+        correctionRight = ((correctionRight - 1) * correctionFactor) + 1;
 
-    myDrive.tankDrive(correctionLeft, correctionRight);
+        myDrive.tankDrive(correctionLeft, correctionRight);
+    } else if (step < path.size()) {
+        int[] target = path.get(step);
+
+        double substep = (now % clock) / clock; // % of the way through current path step
+
+        double targetLeft = target[0] * substep;
+        int realLeft = encoder1.get();
+        if (realLeft == 0)
+            realLeft = 1;
+        double correctionLeft = targetLeft / realLeft;
+        correctionLeft = ((correctionLeft - 1) * correctionFactor) + 1;
+
+        double targetRight = target[1] * substep;
+        int realRight = encoder2.get();
+        if (realRight == 0)
+            realRight = 1;
+        double correctionRight = targetRight / realRight;
+        correctionRight = ((correctionRight - 1) * correctionFactor) + 1;
+
+        myDrive.tankDrive(correctionLeft, correctionRight);
+    } else {
+        myDrive.tankDrive(0, 0);
+    }
 }
